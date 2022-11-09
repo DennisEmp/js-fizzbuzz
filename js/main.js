27d522faa818f4f1a2120990fa6b918eb08dda09
Dennis Empirio <dennis.empirio@gmail.com>
2022-11-09 16:53:36 +0100

    finito

--- js/main.js
+++ js/main.js
@@ -1,24 +1,38 @@
 // tabella numeri 1 a 100
-for (let num = 1; num <= 100; num ++) {
-    console.log("box",num)
+for (let num = 1; num <= 98; num ++) {
     
+    const table = document.querySelector(".table");
+    const box = document.createElement("div");
+    box.classList.add("box");
+    // box.append(num);
+    table.append(box);
+
     // variante "FizzBuzz" per multipli sia di 3 che di 5
-    if (num % 3 == 0 && num % 5 == 0)
-    console.log("FIzzBuzz");
+    if (num % 3 == 0 && num % 5 == 0){
+        box.append("FizzBuzz");
+        console.log("FizzBuzz");
+        box.classList.add("FizzBuzz");
+
+    }
+
     // variante "Fizz" per i multipli di 3
-    else if (num % 3 == 0) 
-    console.log("Fizz");
+    else if (num % 3 == 0){
+        box.append("Fizz");
+        console.log("Fizz");
+        box.classList.add("Fizz");
+
+    }
+
     // variante "Buzz" per i multipli di 5
-    else if (num % 5 == 0) 
-    console.log("Buzz");
-    
+    else if (num % 5 == 0){
+        box.append("Buzz"); 
+        console.log("Buzz");
+        box.classList.add("Buzz");
+
+    }
+
+    else {
+        box.append(num);
+    }
+
 }
-
-
-
-
-
-// else if (i % 15 == 0) console.log("FizzBuzz");
-    
-    
-//     else console.log(i);
